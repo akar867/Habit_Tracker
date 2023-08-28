50a565e93eeb55f0502002a10dd488d1d1886df9
akar867 <137031257+akar867@users.noreply.github.com>
2023-08-28 10:43:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(session({
     },
     store: MongoStore.create(
         {
-            mongoUrl:MONGODB_URL,
+            mongoUrl:'mongodb://127.0.0.1:27017/habit_tracker',
             autoRemover : 'disabled'
         },
         function(err){
